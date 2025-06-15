a4d8fd642412eecf9a2e5627e9cf86d70a15a6d7
boykotla <boykothaktir@gmail.com>
2025-06-15 18:49:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.m
 import { MindARThree } from 'https://cdn.jsdelivr.net/npm/mind-ar@1.1.4/dist/mindar-image-three.prod.js';
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const mindarThree = new MindARThree({
+
+  const mindarThree = new window.MindARThree({
     container: document.body,
     imageTargetSrc: "./assets/gokmedrese-marker.mind",
   });
+
   const { renderer, scene, camera } = mindarThree;
 
   const anchor = mindarThree.addAnchor(0);
